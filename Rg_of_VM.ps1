8129e0f0a0ebe52b35b267ec2157b7f10773346d
RavitejaPatel <43495746+RavitejaPatel@users.noreply.github.com>
2019-12-28 18:25:47 +0530

    Add files via upload
    powershell script which giveS resource groups for which resource type VM's are present

---
+++ Rg_of_VM.ps1
@@ -0,0 +1,31 @@
+﻿cls
+$resourceArray = @()
+
+#$Global:count
+$count=0;
+
+$resourceArray=Get-AzResource
+
+$VMresourceGroups=@()
+
+
+foreach($rgname in $resourceArray)
+{
+if($rgname.ResourceType -eq "Microsoft.Compute/virtualMachines")
+{
+$count+=1;
+$VMresourceGroups+=$rgname.ResourceGroupName
+write-host "no of resource groups contains VM'S are:"$count -ForegroundColor Green
+
+}#if
+}#for
+
+Write-host "------------------------------------------------------------" -ForegroundColor DarkGray
+
+foreach($vmrg in $VMresourceGroups)
+{
+Write-Host "Virtual Machines are present under resource groups" -BackgroundColor Cyan $vmrg -ForegroundColor DarkMagenta
+
+}
+
+
