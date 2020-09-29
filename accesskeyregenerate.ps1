e447648b9bb8efb5945531073296460bebe5fc04
RavitejaPatel <43495746+RavitejaPatel@users.noreply.github.com>
2020-09-29 14:57:51 +0530

    Add files via upload

---
+++ accesskeyregenerate.ps1
@@ -0,0 +1,29 @@
+﻿$storageAccountName="mystuffdisks" 
+$resourceGroup="mystuff"
+
+# retrieve the first storage account key and display it 
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[0]
+
+Write-Host "storage account key 1 = " $storageAccountKey
+
+
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[1]
+
+Write-Host "storage account key 2 = " $storageAccountKey
+
+# re-generate the key1
+New-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName -KeyName key1
+
+# retrieve it again and display key1 it 
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[0] 
+Write-Host "storage account key 1 = " $storageAccountKey
+
+# re-generate the key2
+New-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName -KeyName key2
+
+# retrieve it again and display key2 it 
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroup -Name $storageAccountName).Value[1] 
+Write-Host "storage account key 2 = " $storageAccountKey
+
+
+
