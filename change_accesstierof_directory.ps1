6e3ee084fcd525e511a038d639a71b8fd6e22662
RavitejaPatel <43495746+RavitejaPatel@users.noreply.github.com>
2020-09-29 14:59:13 +0530

    Add files via upload

---
+++ change_accesstierof_directory.ps1
@@ -0,0 +1,44 @@
+﻿Function set-blobarchive
+{
+[CmdletBinding()]
+
+Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $StgAcc,
+         [Parameter(Mandatory=$true, Position=1)]
+          [string]$StgKey,
+         [Parameter(Mandatory=$true, Position=2)]
+         [string] $container_name,
+         [Parameter(Mandatory=$true, Position=3)]
+         [string] $prefix
+         
+    )
+
+
+
+PROCESS
+{
+
+
+
+$ctx = New-AzStorageContext -StorageAccountName $StgAcc -StorageAccountKey $StgKey
+
+
+#Get all the blobs in container
+$blobs = Get-AzStorageBlob -Container $container_name -Context $ctx -Prefix $prefix
+
+$totalblob=@($blobs)
+
+ForEach ($tb in $totalblob) {
+
+ $tb.icloudblob.setstandardblobtier("Archive")
+
+ }
+
+} #PROCESS
+
+
+} #Function
+
+
