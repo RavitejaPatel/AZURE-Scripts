f3a05334d913725602485ec6cfa6780f4d1db280
RavitejaPatel <43495746+RavitejaPatel@users.noreply.github.com>
2020-09-29 15:02:42 +0530

    Add files via upload

---
+++ exec_time_script.ps1
@@ -0,0 +1,16 @@
+﻿$TimeSpan = New-TimeSpan -Minutes 1
+$EndTime = (Get-Date).AddMinutes($TimeSpan.TotalMinutes).ToString("HH:mm")
+
+## START TIMED LOOP
+cls
+do
+{
+## START YOUR SCRIPT
+Write-Warning "Test-Job 1...2...3..."
+Start-Sleep 3
+Write-Warning "End Time = $EndTime`n"
+}
+until ($EndTime -eq (Get-Date -Format HH:mm))
+
+## TIME REACHED AND END SCRIPT
+Write-Host "End Time reached!" -ForegroundColor Green
