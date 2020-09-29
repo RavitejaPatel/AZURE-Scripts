1be5c300db259e9f49d4d9c63d78dc207a0c34ef
RavitejaPatel <43495746+RavitejaPatel@users.noreply.github.com>
2020-09-29 14:58:30 +0530

    Add files via upload

---
+++ change_accesstier_of_selected_blob_from_nestedFolder_undercontainer - Copy.ps1
@@ -0,0 +1,49 @@
+﻿#script to change access tiers of selected blobs
+$StgAcc = "sTRGaCCnAME"
+$StgKey = "Accesskey"
+$Container = CONTANER NAME
+$ctx = New-AzStorageContext -StorageAccountName $StgAcc -StorageAccountKey $StgKey
+
+
+#Get all the blobsobjects in container
+$blobs = Get-AzStorageBlob -Container $Container -Context $ctx
+$totalblob=@($blobs)
+
+#assign blob names with extensions in array2
+$array2 = @('Microsoft.Azure.PowerShell.Clients.Storage.Management.dll','numdest.txt','Microsoft.Win32.Registry.dll','Microsoft.Azure.PowerShell.Strategies.dll','raviteja.docx')
+$final=@()
+$names=@()
+
+ForEach ($tb in $totalblob)
+   {
+
+      $blobname=($tb.name).split("/")
+      $index=$blobname.count - 1 
+      $names=$names+$blobname.getvalue($index) 
+
+
+    ForEach ($a2 in $array2) 
+        {
+
+  
+          if (($names -eq $a2) -and ($tb.name -like "*$a2") ) 
+           {
+            
+              write-host -----------------------------$a2---------------------------
+              $final=$final+$tb
+            }
+
+
+          }#innerfor
+      }#outerfor
+
+      
+
+  Write-Warning "are you ready to change the access tiers of ABOVE blobs??" -WarningAction Inquire
+
+
+          foreach($each in $final)
+          {
+              $each.icloudblob.setstandardblobtier("cool") 
+
+          }
